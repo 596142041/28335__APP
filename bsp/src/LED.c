c01c7027382ee5ba4cdbfaabca5b349ff5d6e0a4
596142041 <wcj53636746@126.com>
2017-07-21 22:41:58 +0800

    更改LED闪烁时间,用于测试上位机软件工作是否正常

--- bsp/src/LED.c
+++ bsp/src/LED.c
@@ -9,6 +9,7 @@ void LED_GPIO_Config(void)
 {
 	   EALLOW;
 		   GpioCtrlRegs.GPBMUX1.bit.GPIO34 = 0;
+		   GpioCtrlRegs.GPAMUX1.bit.GPIO0 = 0;
 		   GpioCtrlRegs.GPBMUX2.bit.GPIO58 = 0;
 		   GpioCtrlRegs.GPBMUX2.bit.GPIO59 = 0x00;
 		   GpioCtrlRegs.GPBMUX2.bit.GPIO60 = 0;
@@ -18,8 +19,10 @@ void LED_GPIO_Config(void)
 		   GpioCtrlRegs.GPBDIR.bit.GPIO60 = 1;
 		   GpioCtrlRegs.GPBDIR.bit.GPIO61 = 1;
 		   GpioCtrlRegs.GPBDIR.bit.GPIO34 = 1;
+		   GpioCtrlRegs.GPADIR.bit.GPIO0 = 1;
 	   EDIS;
 	   GpioDataRegs.GPBCLEAR.all = 0xFFFFFFFF;
+	   GpioDataRegs.GPASET.bit.GPIO0 = 1;
 }
 void LED_Timer_Config(void)
 {
@@ -27,14 +30,15 @@ void LED_Timer_Config(void)
 		SysCtrlRegs.PCLKCR3.bit.CPUTIMER0ENCLK = 1;
 	EDIS;
 	InitCpuTimers();
-	ConfigCpuTimer(&CpuTimer0, 150, 500000);
+	ConfigCpuTimer(&CpuTimer0, 150, 200000);
 	CpuTimer0Regs.TCR.all = 0x4001; // Use write-only instruction to set TSS bit = 0
 
 }
 interrupt void cpu_timer0_isr(void)
 {
    CpuTimer0.InterruptCount++;
-   GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1; // Toggle GPIO32 once per 500 milliseconds
+   GpioDataRegs.GPBTOGGLE.bit.GPIO34 = 1;
+   GpioDataRegs.GPATOGGLE.bit.GPIO0 = 1;
    // Acknowledge this interrupt to receive more interrupts from group 1
    PieCtrlRegs.PIEACK.all = PIEACK_GROUP1;
 }
